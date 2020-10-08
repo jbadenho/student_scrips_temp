f44519aac5e5c761f4e4890cfc4aef1493e55997
NTT-Intern <NTT-Intern>
2020-10-08 13:15:00 +0200

    Update update_reviews_info.sh

--- update_reviews_info.sh
+++ update_reviews_info.sh
@@ -1,5 +1,2 @@
 rm -rf all_details
-wtc-lms reviews | grep "review_details" >> all_details.sh
-chmod +x all_details.sh
-./all_details.sh >> all_details
-rm all_details.sh
+wtc-lms reviews | grep -A 1 "Grade" | grep "details" | sh >> all_details
